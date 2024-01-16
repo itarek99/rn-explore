265919a84279c5d5e0b481c81d8dbccad19f7bf0
Tarekul Islam <tarek.webappick@gmail.com>
2024-01-16 18:04:50 +0600

    New Branch Created

--- App.jsx
+++ App.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
-import FlatColor from './components/FlatColor/FlatColor';
-import Header from './components/Header/Header';
-import ImageCard from './components/ImageCard/ImageCard';
-import ScrollCards from './components/ScrollColors/ScrollCards';
+import {Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Header />
-      <ScrollView>
-        <FlatColor />
-        <ScrollCards />
-        <ImageCard />
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 };
 
