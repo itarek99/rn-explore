20b3d88497a78cf2e06ce22a3d02d39f9b731ecb
Tarekul Islam <tarek.webappick@gmail.com>
2024-01-18 16:23:32 +0600

    Currency Converter App Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,103 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import CurrencySwitch from './components/CurrencySwitch';
+import {currencyByTaka} from './data/currency';
 
 const App = () => {
+  const [convertedCurrency, setConvertedCurrency] = useState('0');
+  const [inputCurrency, setInputCurrency] = useState('0');
+  const [selectedCurrency, setSelectedCurrency] = useState(currencyByTaka[0]);
+
+  const handleCurrencyChange = item => {
+    setSelectedCurrency(item);
+    setConvertedCurrency(
+      (Number(inputCurrency) * Number(item.value)).toFixed(2),
+    );
+  };
+
+  const handleInputChange = value => {
+    setInputCurrency(value);
+    setConvertedCurrency(
+      (Number(value) * Number(selectedCurrency.value)).toFixed(2),
+    );
+  };
+
+  const handleOutputChange = value => {
+    setConvertedCurrency(value);
+    setInputCurrency(
+      (Number(value) / Number(selectedCurrency.value)).toFixed(2),
+    );
+  };
+
+  const renderItem = ({item}) => (
+    <CurrencySwitch
+      selectedCurrency={selectedCurrency}
+      onPress={handleCurrencyChange}
+      item={item}
+    />
+  );
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>TAKA</Text>
+          <TextInput
+            keyboardType="decimal-pad"
+            style={styles.input}
+            onChangeText={handleInputChange}
+            value={String(inputCurrency)}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>{selectedCurrency.name}</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleOutputChange}
+            value={String(convertedCurrency)}
+          />
+        </View>
+      </View>
+      <FlatList
+        data={currencyByTaka}
+        renderItem={renderItem}
+        keyExtractor={item => item.name}
+        contentContainerStyle={styles.contentContainerStyle}
+      />
+    </SafeAreaView>
   );
 };
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainerStyle: {
+    padding: 10,
+    gap: 10,
+  },
+  inputContainer: {
+    margin: 10,
+    marginTop: 0,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    backgroundColor: 'lightgray',
+    borderRadius: 4,
+    fontSize: 20,
+    padding: 10,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default App;
