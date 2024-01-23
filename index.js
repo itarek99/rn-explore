3788bfaa543ef56c2a69f643270795f0d9daad7d
Tarekul Islam <tarek.webappick@gmail.com>
2024-01-23 18:27:07 +0600

    New Branch Created For Navigation

--- index.js
+++ index.js
@@ -3,11 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import TrackPlayer from 'react-native-track-player';
 import {name as appName} from './app.json';
 import App from './src/App';
 
-import playerService from './playerServices';
-
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => playerService);
--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
-import {addTrack, setupPlayer} from '../playerServices';
-import MusicPlayer from './screens/MusicPlayer';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  const [isPlayerReady, setIsPlayerReady] = useState(false);
-
-  const setup = async () => {
-    const isSetup = await setupPlayer();
-    if (isSetup) {
-      await addTrack();
-    }
-    setIsPlayerReady(isSetup);
-  };
-
-  useEffect(() => {
-    setup();
-  }, []);
-
-  if (!isPlayerReady) {
-    return (
-      <SafeAreaView style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </SafeAreaView>
-    );
-  }
-
   return (
-    <View style={styles.container}>
-      <MusicPlayer />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#242333',
-  },
-  loadingContainer: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({});
