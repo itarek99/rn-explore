eddf948dc9acadac30dcb22d80160b76d53bdcf0
Tarekul Islam <tarek.webappick@gmail.com>
2024-01-25 13:02:38 +0600

    New Branch Created For WP Rest API

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import About from './screens/Details/Details';
-import Home from './screens/Home/Home';
-
-const Stack = createNativeStackNavigator();
+import {StyleSheet, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Product List',
-          }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={About}
-          options={{
-            title: 'Product Details',
-            headerBackTitle: 'Back',
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
   );
 };
+
 export default App;
+
+const styles = StyleSheet.create({});
